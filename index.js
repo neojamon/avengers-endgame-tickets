f64201e8bbeaafca0c407fe6b2ea636cdea5de8f
Raul Martin Reyero <rmreyero@bitban.com>
2019-02-27 18:47:04 +0100

    Added app port and listen

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
 app.get('/', function(req, res) {
   res.send('give me my avengers tickets!');
+});
+
+app.listen(PORT, function () {
+  console.log(`APP listening on port ${PORT}!`);
 });
 
