06862c7952c7475f850ab0d552435d1b3fff6717
Raul Martin Reyero <rmreyero@bitban.com>
2019-02-28 01:44:35 +0100

    Added home endpoint

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const hbs = require( 'express-handlebars');
+const jobs = require('./jobs');
 
 const PORT = process.env.PORT || 8080;
 
-app.get('/', function(req, res) {
-  res.send('give me my avengers tickets!');
+// view engine setup
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+app.engine( 'hbs', hbs( {
+  extname: 'hbs',
+  defaultView: 'default',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
+
+app.get('/', async function(req, res) {
+  const result = await jobs.cinesa();
+  res.render('home', { layout: 'default', result: JSON.stringify(result,null, 2) });
 });
 
 app.listen(PORT, function () {
